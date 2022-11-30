bed1ee8cd509249eb92438fb4a43fe8f81c2dfb2
Tal Ben-Horin PhD <tbenhor@ncsu.edu>
2022-11-30 16:47:34 -0500

    UpdateR

--- NERRfilter.R
+++ NERRfilter.R
@@ -10,6 +10,5 @@ set.seed(1234)
 sals <- read.csv("LoosinCreek.csv", fill = FALSE, header = TRUE) 
 sals = select(sals, 2,3)
 
-
 ggplot(data = sals, aes(x = DateTimeStamp, y = Sal)) +
   geom_point() 
